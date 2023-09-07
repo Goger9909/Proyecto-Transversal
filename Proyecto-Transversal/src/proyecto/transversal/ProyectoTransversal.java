eae0126144f86841d164eb1f67c9862423dc5977
Goger9909 <123704792+Goger9909@users.noreply.github.com>
2023-09-07 11:20:00 -0300

    Add files via upload

---
+++ Proyecto-Transversal/src/proyecto/transversal/ProyectoTransversal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.transversal;
+
+/**
+ *
+ * @author MiPc
+ */
+public class ProyectoTransversal {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        String holass;
+    }
+    
+}
