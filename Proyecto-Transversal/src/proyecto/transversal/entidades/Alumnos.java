1e3125529b1ddae808fe3a3b1d26ff973fac48b7
ferben007 <141643647+ferben007@users.noreply.github.com>
2023-09-07 12:15:51 -0300

    agregar pak

---
+++ Proyecto-Transversal/src/proyecto/transversal/entidades/Alumnos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.transversal.entidades;
+
+/**
+ *
+ * @author ferben007
+ */
+public class Alumnos {
+    private int idAlumnos;
+}
