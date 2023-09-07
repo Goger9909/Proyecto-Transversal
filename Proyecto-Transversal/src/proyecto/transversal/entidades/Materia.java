04b5f890a3b0ca01b1662dac426a57a85a4d65ae
ferben007 <141643647+ferben007@users.noreply.github.com>
2023-09-07 12:26:53 -0300

    clase materia

---
+++ Proyecto-Transversal/src/proyecto/transversal/entidades/Materia.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.transversal.entidades;
+
+/**
+ *
+ * @author ferben007
+ */
+public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int anioMateria;
+    private boolean activo;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, String nombre, int anioMateria, boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public Materia(String nombre, int anioMateria, boolean activo) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia " + "id Materia: " + idMateria + ", nombre: " + nombre + ", año Materia: " + anioMateria ;
+    }
+    
+    
+}
