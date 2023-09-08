169dd2fadfeca28c99945ec1c6b1fab3c78d3d67
yamiilaalejandra <136135024+yamiilaalejandra@users.noreply.github.com>
2023-09-08 18:07:41 -0300

    Update MateriaData.java

--- Proyecto-Transversal/src/proyecto/transversal/accesoADatos/MateriaData.java
+++ Proyecto-Transversal/src/proyecto/transversal/accesoADatos/MateriaData.java
@@ -5,10 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
-import org.mariadb.jdbc.Statement;
 import proyecto.transversal.entidades.Materia;
 
 
