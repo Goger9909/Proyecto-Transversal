3565fa7282d5f5c72d724f4115283e3f116aa1be
MatiV14 <matias993@gmail.com>
2023-09-08 14:08:04 -0300

    parte de Materia

--- Proyecto-Transversal/src/proyecto/transversal/ProyectoTransversal.java
+++ Proyecto-Transversal/src/proyecto/transversal/ProyectoTransversal.java
@@ -3,6 +3,8 @@ package proyecto.transversal;
 
 import java.sql.Connection;
 import proyecto.transversal.accesoADatos.Conexion;
+import proyecto.transversal.accesoADatos.MateriaData;
+import proyecto.transversal.entidades.Materia;
 
 
 public class ProyectoTransversal {
@@ -15,6 +17,18 @@ public class ProyectoTransversal {
         String ComoEstas;
         String Hola; 
         Connection con = Conexion.getConexion();
-    }
     
+////    CARGAR MATERIA
+//    Materia biologia= new Materia("Biologia",1,true);
+//    MateriaData mate=new MateriaData();
+//    mate.guardarMateria(biologia);
+
+////    MODIFICAR MATERIA
+//mate.modificarMateria(biologia);
+
+    
+    
+    
+    
+    }
 }
