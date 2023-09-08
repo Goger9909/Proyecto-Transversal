a4eb988aede94b202a54ea7d91a20b30118bec3d
ferben007 <141643647+ferben007@users.noreply.github.com>
2023-09-08 11:09:47 -0300

    dni

--- Proyecto-Transversal/src/proyecto/transversal/accesoADatos/AlumnoData.java
+++ Proyecto-Transversal/src/proyecto/transversal/accesoADatos/AlumnoData.java
@@ -6,6 +6,12 @@
 package proyecto.transversal.accesoADatos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import proyecto.transversal.entidades.Alumno;
 
 /**
@@ -13,12 +19,23 @@ import proyecto.transversal.entidades.Alumno;
  * @author MiPc
  */
 public class AlumnoData {
+
     private Connection con = null;
-    
-    public AlumnoData(){
+
+    public AlumnoData() {
         con = Conexion.getConexion();
     }
-    public void agregarAlumno(Alumno alumno){
+
+    public void agregarAlumno(Alumno alumno) {
         String sql = "INSERT INTO (dni, apellido, nombre, fechaNacimiento, estado) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1, alumno.getDni());
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno" + ex.getMessage());
+        }
+
     }
 }
