452b566f180d6d15aa3f057b25aca60a3b67bac5
yamiilaalejandra <136135024+yamiilaalejandra@users.noreply.github.com>
2023-09-12 15:09:33 -0300

    Update ProyectoTransversal.java

--- Proyecto-Transversal/src/proyecto/transversal/ProyectoTransversal.java
+++ Proyecto-Transversal/src/proyecto/transversal/ProyectoTransversal.java
@@ -6,7 +6,11 @@ import java.time.LocalDate;
 import java.time.Month;
 import proyecto.transversal.accesoADatos.AlumnoData;
 import proyecto.transversal.accesoADatos.Conexion;
+import proyecto.transversal.accesoADatos.InscripcionData;
+import proyecto.transversal.accesoADatos.MateriaData;
 import proyecto.transversal.entidades.Alumno;
+import proyecto.transversal.entidades.Inscripcion;
+import proyecto.transversal.entidades.Materia;
 
 
 
@@ -20,12 +24,12 @@ public class ProyectoTransversal {
         String ComoEstas;
         String Hola; 
 //        Connection con = Conexion.getConexion();
-    Alumno al = new Alumno(44444444, "Suarez", "Fernando", LocalDate.of(1990, Month.MARCH, 25), true);
+    Alumno al = new Alumno(1,44444444, "Suarez", "Fernando", LocalDate.of(1990, Month.MARCH, 25), true);
     AlumnoData ad = new AlumnoData();
     ad.agregarAlumno(al);
 
 ////    CARGAR MATERIA
-//    Materia biologia= new Materia("Biologia",1,true);
+//   Materia biologia= new Materia(1,"Biologia",1,true);
 //    MateriaData mate=new MateriaData();
 //    mate.guardarMateria(biologia);
 
@@ -57,7 +61,10 @@ public class ProyectoTransversal {
 //    System.out.println("----------");
 //}    
     
-    
-    
+//CARGAR INSCRIPCION 
+//  Inscripcion ins = new Inscripcion(al , biologia, 8);
+//  InscripcionData insc = new InscripcionData();
+//  insc.GuardarAlumno(ins);
+   
     }
 }
