dbc68494d29f70dbd3c9565dabd8970a8500e350
yamiilaalejandra <136135024+yamiilaalejandra@users.noreply.github.com>
2023-09-14 00:09:13 -0300

    Update ProyectoTransversal.java

--- Proyecto-Transversal/src/proyecto/transversal/ProyectoTransversal.java
+++ Proyecto-Transversal/src/proyecto/transversal/ProyectoTransversal.java
@@ -22,7 +22,7 @@ public class ProyectoTransversal {
      
      Connection con = Conexion.getConexion();
      
-    Alumno al = new Alumno(38495098, "Perez", "Paola", LocalDate.of(1990, Month.MARCH, 25), true);
+//    Alumno al = new Alumno(38495098, "Perez", "Paola", LocalDate.of(1990, Month.MARCH, 25), true);
     AlumnoData ad = new AlumnoData();
  //   ad.agregarAlumno(al);
 
@@ -68,13 +68,13 @@ public class ProyectoTransversal {
   MateriaData md = new MateriaData();
 
   //BUSCAMOS ALUMNO Y MATERIA POR ID 
-   Alumno a =  da.buscarAlumnoPorID(9);
-   Materia e =  md.buscarMateria(6);
+//   Alumno a =  da.buscarAlumnoPorID(9);
+//   Materia e =  md.buscarMateria(6);
    
    //CREAMOS UNA INSCRIPCION 
-   Inscripcion ins = new Inscripcion(a, e, 8);
+//   Inscripcion ins = new Inscripcion(a, e, 8);
    
- //CARGAR INSCRIPCION 
+ //CARGAR INSCRIPCION.
   // insc.GuardarInscripcion(ins);
 
 
@@ -82,8 +82,20 @@ public class ProyectoTransversal {
 //insc.ActualizarNota(7, 6, 10);
 
 //OBTENER MATERIAS CURSADAS POR ALUMNO 
-//   insc.ObtenerMateriasCursadas(1);
+// insc.ObtenerMateriasCursadas(1);
 
+//BORRAR INSCRIPCION POR IDALUMNO Y IDMATERIA.
+//insc.BorrarInscripcionMateriaAlumno(1, 4);
 //
+//for (Inscripcion inscrip : insc.ObtenerInscripciones()){
+//     System.out.println("-------------------------------------");
+//     System.out.println("IdInscripcion: "+inscrip.getIdIncripcion());
+//     System.out.println("Apellido: "+inscrip.getAlumno().getApellido());
+//     System.out.println("Materia "+inscrip.getMateria().getNombre());
+//}
+
+//OBTENER MATERIAS CURSADAS 
+ insc.ObtenerInscripcionesPorAlumno(1); 
+
     }
 }
