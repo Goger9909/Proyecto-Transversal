5b16280e0c146b0414c8ca4ac22c504aff50f367
ferben007 <141643647+ferben007@users.noreply.github.com>
2023-09-19 11:33:20 -0300

    cargama

--- Proyecto-Transversal/src/proyecto/transversal/vista/Main.java
+++ Proyecto-Transversal/src/proyecto/transversal/vista/Main.java
@@ -80,6 +80,11 @@ public class Main extends javax.swing.JFrame {
         jMenu3.add(jMenuItem3);
 
         jMenuItem4.setText("Manipulacion de Notas");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
         jMenu3.add(jMenuItem4);
 
         jMenuBar1.add(jMenu3);
@@ -171,6 +176,14 @@ public class Main extends javax.swing.JFrame {
             System.exit(0);
         }
     }//GEN-LAST:event_jMenuItem6ActionPerformed
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        // TODO add your handling code here:
+        CargaDeNotas ins = new CargaDeNotas(); 
+        ins.setVisible(true);
+        jEscritorio.add(ins);
+        jEscritorio.moveToFront(ins);
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     
     public static void main(String args[]) {
