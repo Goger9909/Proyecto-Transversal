a0ba101c4fa38014b898d20934c398b12b9b6361
yamiilaalejandra <136135024+yamiilaalejandra@users.noreply.github.com>
2023-09-25 20:03:45 -0300

    Update Main.java

--- Proyecto-Transversal/src/proyecto/transversal/vista/Main.java
+++ Proyecto-Transversal/src/proyecto/transversal/vista/Main.java
@@ -153,6 +153,7 @@ public class Main extends javax.swing.JFrame {
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         Vista_Materia GA = new Vista_Materia();
+        GA.getContentPane().setBackground(new Color (0,102,255));
         GA.setVisible(true);
         jEscritorio.add(GA);
         jEscritorio.moveToFront(GA);
